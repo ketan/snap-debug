1d03aeca36fdc629d34b82508cc4cc4473bd4848
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2014-07-16 22:36:23 -0300

    Add a proper client that can manage process execution

--- client.js
+++ client.js
@@ -1,11 +1,32 @@
 console.log(process.env.URL)
 var socket = require('socket.io-client')(process.env.URL);
+var spawn = require('child_process').spawn;
+var sys = require('sys')
 
 socket.on('connect', function() {
   console.log('connected...');
 
-  socket.on('command-to-run', function(data) {
-    console.log('got event' + data)
+  socket.on('command-to-run', function(command) {
+    console.log(' $ ' + command)
+
+    var cmd = spawn("bash", ["-c", command]);
+
+    cmd.stdout.on('data', function(data) {
+      sys.print(data);
+    });
+
+    cmd.stderr.on('data', function(data) {
+      sys.print(data);
+    });
+
+    cmd.on('close', function(code) {
+      console.log('child process exited with code ' + code);
+    });
+
+    cmd.on('error', function(err){
+      console.log(err)
+    })
+
   });
 
   socket.on('disconnect', function() {
